9329d99b3e4c26f06087f34056beefe7937115f3
Ekaterina <eka.kachan@gmail.com>
2022-06-15 20:06:28 +0400

    fix: fix url

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ const requestListener = function (req, res) {
   if (code) {
     const client_id = "e86051972558ac68b5a8";
     const client_secret = "653e710d96581d947159bcf5063015aa105f759d";
-    const redirect_uri = "https://main--github-test-project.netlify.app";
+    const redirect_uri = "https://main--github-test-project.netlify.app/repos";
 
     request.post({
         url: `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${redirect_uri}`,
@@ -40,4 +40,3 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener);
 server.listen(8000);
-console.log("Server listen http://localhost:8000");
